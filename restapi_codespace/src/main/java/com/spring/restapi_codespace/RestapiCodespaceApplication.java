66e6071815862bccb90a647b94dcea5bdc7bf72e
Deepak Kumar <126335641+Deepak-Kumar45@users.noreply.github.com>
2023-09-26 10:15:31 +0000

    created spring boot project using codespace

---
+++ restapi_codespace/src/main/java/com/spring/restapi_codespace/RestapiCodespaceApplication.java
@@ -0,0 +1,14 @@
+package com.spring.restapi_codespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiCodespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiCodespaceApplication.class, args);
+		System.out.println("\n\t\t!!..Application has been started..!!");
+	}
+
+}
---
+++ restapi_codespace/src/test/java/com/spring/restapi_codespace/RestapiCodespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.restapi_codespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiCodespaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
